2fc3879d88d72bfc1a7a5c7046a9d0819071941f
alitkbbl <alitaghi8305@gmail.com>
2026-01-20 13:12:20 +0330

    tedad jaighasht ok

--- season7/tedad_jaigasht.py
+++ season7/tedad_jaigasht.py
@@ -1,2 +1,21 @@
 n, k = map(int, input().split())
+result = 0
 
+def solve(arr):
+    global result
+    if len(arr) == n:
+        count = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if arr[i] > arr[j]:
+                    count += 1
+        if count == k:
+            result += 1
+        return
+
+    for i in range(1, n + 1):
+        if i not in arr:
+            solve(arr + [i])
+
+solve([])
+print(result)
